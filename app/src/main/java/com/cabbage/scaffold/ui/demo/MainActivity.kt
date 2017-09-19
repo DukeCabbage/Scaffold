156be4d521515b9ebace5983f6739b6b389356ab
DukeCabbage <liaowng@gmail.com>
2017-09-19 01:21:47 -0700

    Tweaking gradle file

--- app/src/main/java/com/cabbage/scaffold/ui/demo/MainActivity.kt
+++ app/src/main/java/com/cabbage/scaffold/ui/demo/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.cabbage.scaffold.ui.demo
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.v7.widget.Toolbar
 import android.widget.Toast
-import butterknife.BindView
 import butterknife.ButterKnife
+import com.cabbage.scaffold.BuildConfig
 import com.cabbage.scaffold.R
 import com.cabbage.scaffold.ui.base.BaseActivity
 import com.cabbage.scaffold.ui.mvp.MvpView
@@ -13,6 +11,8 @@ import com.jakewharton.rxbinding2.view.RxView
 import com.tbruyelle.rxpermissions2.RxPermissions
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -21,9 +21,6 @@ class MainActivity : BaseActivity(), MvpView {
     @Inject lateinit var presenter: MainPresenter
     @Inject lateinit var rxPermission: RxPermissions
 
-    @BindView(R.id.toolbar) lateinit var mToolbar: Toolbar
-    @BindView(R.id.fab) lateinit var mFab: FloatingActionButton
-
     var clickSub: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +28,12 @@ class MainActivity : BaseActivity(), MvpView {
         Timber.v("onCreate")
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this)
-        setSupportActionBar(mToolbar)
+        setSupportActionBar(toolbar)
 
         activityComponent.inject(this)
+
+        tvVersionName.text = BuildConfig.VERSION_NAME
+        tvVersionCode.text = "${BuildConfig.VERSION_CODE}"
     }
 
     public override fun onStart() {
@@ -42,7 +42,7 @@ class MainActivity : BaseActivity(), MvpView {
         presenter.mvpView = this
         presenter.ensureLocationPermission(rxPermission)
 
-        clickSub = RxView.clicks(mFab).subscribeBy(onNext = { presenter.ensureLocationPermission(rxPermission) })
+        clickSub = RxView.clicks(fab).subscribeBy(onNext = { presenter.ensureLocationPermission(rxPermission) })
     }
 
     public override fun onStop() {
