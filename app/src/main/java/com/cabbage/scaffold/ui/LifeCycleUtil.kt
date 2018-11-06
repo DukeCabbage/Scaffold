5fd7fd39b4fb2acec636ec4d1fa4b9ba9a1f7243
liaowng <liaowng@gmail.com>
2018-11-06 10:43:31 -0800

    Comments

--- app/src/main/java/com/cabbage/scaffold/ui/LifeCycleUtil.kt
+++ app/src/main/java/com/cabbage/scaffold/ui/LifeCycleUtil.kt
@@ -8,7 +8,7 @@ import android.support.v4.app.FragmentActivity
 import timber.log.Timber
 
 /**
- * Here are utility functions related to arch livedata and arch viewmodel
+ * Here are utility functions related to arch live data and arch view model
  */
 
 typealias daggerLazy<T> = dagger.Lazy<T>
@@ -17,31 +17,46 @@ typealias VMFac = ViewModelProvider.Factory
 
 /**
  * Takes a lazy [ViewModelProvider.Factory], only unwraps it if view model wasn't created before.
- * This is only relevant for ViewModels whose constructors require arguments, thus making it necessary
- * for us to implement custom [ViewModelProvider.Factory]
+ * This is only relevant for ViewModels whose constructors require arguments, thus making it
+ * necessary for us to implement custom [ViewModelProvider.Factory]
  */
 inline fun <reified T : ViewModel>
-        FragmentActivity.getViewModel(lazyFactory: daggerLazy<out VMFac>): T {
-    return try {
-        Timber.v("Provide VM cached")
-        getViewModel<T>(null)
-    } catch (e: Throwable) {
-        Timber.v("Provide VM with factory")
-        getViewModel(lazyFactory.get())
-    }
-}
+        FragmentActivity.getViewModel(lazyFactory: daggerLazy<out VMFac>): T =
+        try {
+            Timber.v("Provide VM cached")
+            getViewModel<T>(null)
+        } catch (e: Throwable) {
+            Timber.v("Provide VM with factory")
+            getViewModel(lazyFactory.get())
+        }
 
-inline fun <reified T : ViewModel> FragmentActivity.getViewModel(factory: VMFac? = null): T =
+/**
+ * Get [ViewModel] of type [T], with optional [factory]
+ */
+inline fun <reified T : ViewModel>
+        FragmentActivity.getViewModel(factory: VMFac? = null): T =
         ViewModelProviders.of(this, factory)[T::class.java]
 
 
-// We need the one associated with host activity, not fragment itself
-// Calling ViewModelProviders.of(this: Fragment) will result in a distinct instance
-inline fun <reified T : ViewModel> Fragment.getViewModel(factory: VMFac? = null): T? =
+// We need the view model associated with host activity, not fragment itself
+// Calling ViewModelProviders.of(this: Fragment) will result in a different instance
+/**
+ * Get [ViewModel] of type [T] from the view model pool of the activity this fragment is attached to.
+ * Returns null if activity is not attached yet.
+ */
+inline fun <reified T : ViewModel>
+        Fragment.getViewModel(factory: VMFac? = null): T? =
         activity?.run { getViewModel(factory) }
 
-inline fun <reified T : ViewModel> Fragment.getViewModel(lazyFactory: daggerLazy<out VMFac>): T? =
+/**
+ * Fragment version of [getViewModel] with lazy factory. Returns null if activity is not attached yet.
+ */
+inline fun <reified T : ViewModel>
+        Fragment.getViewModel(lazyFactory: daggerLazy<out VMFac>): T? =
         activity?.run { getViewModel(lazyFactory) }
 
-
-fun <X, Y> LiveData<X>.map(func: (X) -> Y): LiveData<Y> = Transformations.map(this, func)
+/**
+ * Shortcut for [Transformations.map]
+ */
+fun <X, Y> LiveData<X>.map(func: (X) -> Y): LiveData<Y> =
+        Transformations.map(this, func)
